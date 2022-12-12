f79defe410fe1b4e79d86e8ee0778ea86a2d49ab
Valmir Santana <valmirsantanape@gmail.com>
2022-12-12 16:21:25 -0300

    update

--- src/compenents/input/styles.js
+++ src/compenents/input/styles.js
@@ -1,12 +1,8 @@
 import styled from "styled-components"
 
 export const ImputContainer = styled.div `
-    width: 175px;
-    height: 30px;
-    background: #2D2D37;
-    border-radius: 8px;
-    padding: 2px 5px;
-    margin: 0 12px;
-
+    background-color: white;
+    height: 63px;
+    width: 100px;
 
 `
