b9f72e40de0bc82f7b1d3c2422f38df5426bdfe4
Valmir Santana <valmirsantanape@gmail.com>
2022-12-13 00:46:09 -0300

    update

--- src/compenents/Header/styles.js
+++ src/compenents/Header/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.div `
     width: 100%;
     height: 47px;
 
-    padding: 0 5px;
+    padding: 0 20px;
     display: flex;
     justify-content: space-between;
     justify-content: space-between;
